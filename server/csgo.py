960fcd742a715dd7e099ac7fdc89c91e843b71de
sippnex <julian.sobotka@gmail.com>
2021-04-08 21:11:36 +0200

    wip

--- server/csgo.py
+++ server/csgo.py
@@ -1,8 +1,9 @@
 import libtmux
 
-def install():
+def install(tmux_session: libtmux.Session):
     print('installing csgo server...')
+    tmux_session.attached_pane.send_keys('~/Projects/github/wgsm/steamcmd/steamcmd.sh +runscript ~/Projects/github/wgsm/server/steamcmd_install_cs16.txt')
 
 def start(tmux_session: libtmux.Session):
     print('starting csgo server...')
-    tmux_session.attached_pane.send_keys('~/github/wgsm/csgo_ds/srcds_run')
+    tmux_session.attached_pane.send_keys('~/Projects/github/wgsm/csgo_ds/srcds_run')
--- wgsm_core.py
+++ wgsm_core.py
@@ -1,14 +1,31 @@
 import libtmux
+import threading
+import time
 from re import search
 
 from server import csgo
 
-tmux_server = libtmux.Server()
+class ObserveThread (threading.Thread):
+    def __init__(self, tmux_server: libtmux.Server):
+      threading.Thread.__init__(self)
+      self.tmux_server = tmux_server
+    def run(self):
+        while (True):
+            sessions = [s for s in self.tmux_server.sessions if search('^wgsm#.*#.*', s.get('session_name'))]
+            for session in sessions:
+                output = session.attached_pane.capture_pane()
+                print('----------------------')
+                print('new output')
+                for line in output:
+                    print(line)
+                print('----------------------')
+            time.sleep(2)
+
 
 def unknown_game(game):
     print(f'unknown game \'{game}\'')
 
-def serer_not_found(server_name):
+def server_not_found(server_name):
     print(f'server with name \'{server_name}\' not found')
 
 def server_already_exists(server_name):
@@ -18,30 +35,36 @@ def game_exists(game):
     return game == 'csgo'
 
 def get_servers():
-    return [s for s in tmux_server.sessions if search('^wgsm-.*', s.get('session_name'))]
-
+    return [s for s in tmux_server.sessions if search('^wgsm#.*#.*', s.get('session_name'))]
+    
 def create_server(server_name, game):
     if not game_exists(game):
         unknown_game(game)
         return
-    if tmux_server.has_session('wgsm-' + server_name):
+    tmux_sessions = [s for s in tmux_server.sessions if search('^wgsm#.*#' + server_name, s.get('session_name'))]
+    if len(tmux_sessions) > 0:
         server_already_exists(server_name)
         return
     print(f'creating server \'{server_name}\'')
-    tmux_session = tmux_server.new_session(session_name='wgsm-' + server_name)
-    start_server(server_name, game)
+    tmux_session = tmux_server.new_session(session_name='wgsm#' + game + '#' + server_name)
+    if game == 'csgo':
+        csgo.install(tmux_session)
 
 def start_server(server_name, game):
     if not game_exists(game):
         unknown_game(game)
         return
     print(f'starting server \'{server_name}\'')
-    tmux_session = None
-    if not tmux_server.has_session('wgsm-' + server_name):
-        serer_not_found(server_name)
+    tmux_sessions = [s for s in tmux_server.sessions if search('^wgsm#.*#' + server_name, s.get('session_name'))]
+    if len(tmux_sessions) == 0:
+        server_not_found(server_name)
         return
-    tmux_session = tmux_server.find_where({'session_name': 'wgsm-' + server_name})
+    tmux_session = tmux_sessions[0]
     if game == 'csgo':
         csgo.start(tmux_session)
+
+tmux_server = libtmux.Server()
+observe_thread = ObserveThread(tmux_server)
+observe_thread.start()
 
 #print(session.attached_pane.capture_pane())
